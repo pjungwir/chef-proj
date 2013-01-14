522101fc3999c93fa071228eb1f30d2fcd8eaadf
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2013-01-14 15:51:10 -0800

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+maintainer        "Paul A. Jungwirth"
+maintainer_email  "pj@illuminatedcomputing.com"
+license           "Apache 2.0"
+description       "Installs PROJ"
+version           "0.1"
+
+recipe "proj", "Installs PROJ"
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
+
